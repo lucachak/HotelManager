11d0451bc311253d2df2beb9c980ca40fb654dc7
lucachak <lucas.lucachak9@gmail.com>
2026-02-12 03:23:36 +0000

    fixing

--- config/urls.py
+++ config/urls.py
@@ -1,28 +1,18 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
-# --- CONFIGURAÇÃO DE IDENTIDADE DO ADMIN ---
-# Isso altera os textos padrão do Django Admin para o nome do seu hotel
 admin.site.site_header = "Hotel Lux | Gestão Administrativa"
 admin.site.site_title = "Hotel Lux"
 admin.site.index_title = "Painel de Controle Técnico"
 
-# O link "Ver Site" no topo do Admin agora levará direto para o seu Dashboard Staff
 admin.site.site_url = '/'
 
 urlpatterns = [
-    # Admin do Django
     path('admin/', admin.site.urls),
-
-    # Concentramos todas as rotas de negócio dentro do apps.core.urls
-    # (Reservas, Financeiro, Hóspedes, etc, já estão inclusos lá)
     path('', include('apps.core.urls')),
-
-    # Rota para autenticação padrão do Django (opcional, caso use login/logout padrão)
     # path('accounts/', include('django.contrib.auth.urls')),
-
 ]
 
 # --- CONFIGURAÇÕES DE DESENVOLVIMENTO (DEBUG) ---
